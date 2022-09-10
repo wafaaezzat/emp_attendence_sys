c4743abf8718e446a5f8422c893422bbe0c2f9a1
wafaaezzat <wafaaezzat45@icloud.com>
2022-09-10 19:46:48 +0200

    migration done

--- emp_attendence_sys/database/migrations/2014_10_12_000000_create_users_table.php
+++ emp_attendence_sys/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('bio')->nullable();
             $table->string('email')->unique();
+            $table->string('email_verified')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
---
+++ emp_attendence_sys/database/migrations/2022_09_10_173806_create_project_attendees_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_attendees', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('project_id')->nullable()->index();
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+            $table->unsignedBigInteger('attendance_id')->nullable()->index();
+            $table->foreign('attendance_id')->references('id')->on('attendances')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_attendees');
+    }
+};
