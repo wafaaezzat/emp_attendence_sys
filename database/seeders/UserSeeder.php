6b1a3ede5443d15e190855b2b30880b0af28f8c0
Ninja Zone <wafaaezzat45@icloud.com>
2022-10-17 13:26:58 +0200

    fix ui auth view

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Database\Factories\UserFactory;
 use Faker\Factory;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -16,6 +17,17 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()->count(10)->create();
+//        User::factory()->count(10)->create();
+        User::insert([
+            'name' => 'wafaa',
+            'phone' => '01122655338',
+            'address' => 'dd jwhgdsjwgd',
+            'bio' => 'cs',
+            'role_id' =>'1',
+            'email' => 'wafaaezzat371@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),]);
+
     }
 }
